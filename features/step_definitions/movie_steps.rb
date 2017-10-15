3be99acaaf87a782a88139536f5a56387a9642cb
Nathan D Smith <nathan-d-smith@uiowa.edu>
2017-10-15 22:21:59 +0000

    still missing search_tmdb view

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -28,7 +28,7 @@ Given /^I am on the RottenPotatoes home page$/ do
    click_on "More about #{title}"
  end
 
- Then /^(?:|I )should see "([^"]*)"$/ do |text|
+ Then /^I should see "(.*?)"$/ do |text|
     expect(page).to have_content(text)
  end
 

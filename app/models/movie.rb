ad55d03573e9251d62b085e35d4a8b5657c711e7
Nathan D Smith <nathan-d-smith@uiowa.edu>
2017-10-15 22:51:03 +0000

    created search_tmdb view

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,6 +7,7 @@ class Movie < ActiveRecord::Base
   
   def self.find_in_tmdb(string)
     begin
+      Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
       Tmdb::Movie.find(string)
     rescue Tmdb::InvalidApiKeyError
         raise Movie::InvalidKeyError, 'Invalid API key'
